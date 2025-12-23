ef74b05115b95f3e82d81b1a170c7eebee89d9a2
YtH0823 <yutonghu@whu.edu.cn>
2025-12-23 19:45:56 +0800

    Add files via upload

---
+++ sample4geo/trainer.py
@@ -0,0 +1,49 @@
+import time
+import torch
+from tqdm import tqdm
+from .utils import AverageMeter
+from torch.cuda.amp import autocast
+import torch.nn.functional as F
+
+
+def predict(train_config, model, dataloader):
+    
+    model.eval()
+    
+    # wait before starting progress bar
+    time.sleep(0.1)
+    
+    if train_config.verbose:
+        bar = tqdm(dataloader, total=len(dataloader))
+    else:
+        bar = dataloader
+        
+    img_features_list = []
+    
+    ids_list = []
+    with torch.no_grad():
+        
+        for img, ids in bar:
+        
+            ids_list.append(ids)
+            
+            with autocast():
+         
+                img = img.to(train_config.device)
+                img_feature = model(img)
+            
+                # normalize is calculated in fp32
+                if train_config.normalize_features:
+                    img_feature = F.normalize(img_feature, dim=-1)
+            
+            # save features in fp32 for sim calculation
+            img_features_list.append(img_feature.to(torch.float32))
+      
+        # keep Features on GPU
+        img_features = torch.cat(img_features_list, dim=0) 
+        ids_list = torch.cat(ids_list, dim=0).to(train_config.device)
+        
+    if train_config.verbose:
+        bar.close()
+        
+    return img_features, ids_list
